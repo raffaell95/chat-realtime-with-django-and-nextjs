920a008a400959d0f5344cb1f618b6ae75d22866
raffaell95 <raffaell95@live.com>
2024-09-11 21:15:05 -0300

    add new feature

--- chats/views/messages.py
+++ chats/views/messages.py
@@ -31,4 +31,47 @@ class ChatMesageView(BaseView):
             }
         })
 
+        messages = ChatMessage.objects.filter(
+            chat_id=chat_id,
+            deleted_at__isnull=True
+        ).order_by('created_at').all()
+
+        serializer = ChatMessageSerializer(messages, many=True)
+
+        socket.emit('update_at', {
+            "query": {
+                "users": [chat.from_user_id, chat.to_user_id]
+            }
+        })
+
+        return Response({
+            "messages": serializer.data
+        })
+    
+    def post(self, request, chat_id):
+        body = request.data.get('body')
+        file = request.FILES.get('file')
+        audio = request.FILES.get('audio')
+
+        chat = self.chat_belongs_to_user(
+            user_id=request.user.id,
+            chat_id=chat_id
+        )
+
+        self.mark_messages_as_seen(chat_id, request.user.id)
+
+        if not body and not file and audio:
+            raise ValidationError("Nenhum parametro foi informado")
         
+        attachment = None
+
+        if file:
+            storage = FileSystemStorage(
+                settings.MEDIA_ROOT / 'files',
+                settings.MEDIA_URL + 'files'
+            )
+
+            content_type = file.content_type
+            name = file.name.split('.')[0]
+            extension = file.name.split('.')[-1]
+            size = file.size
