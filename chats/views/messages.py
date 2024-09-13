aafd7065dbd23940f3352ff1cf0c4397b7097b5f
raffaell95 <raffaell95@live.com>
2024-09-13 17:03:02 -0300

    add delete chat

--- chats/views/messages.py
+++ chats/views/messages.py
@@ -15,7 +15,7 @@ from django.conf import settings
 
 import uuid
 
-class ChatMesageView(BaseView):
+class ChatMessagesView(BaseView):
     def get(self, request, chat_id):
         chat = self.chat_belongs_to_user(
             user_id=request.user.id,
@@ -82,4 +82,87 @@ class ChatMesageView(BaseView):
             file = storage.save(f"{uuid.uuid4()}.{extension}", file)
             src = storage.url(file)
 
-            
+            attachment = FileAttachment.objects.create(
+                name=name,
+                extension=extension,
+                size=size,
+                src=src,
+                content_type=content_type
+            )
+        elif audio:
+            storage = FileSystemStorage(
+                settings.MEDIA_ROOT / 'audio',
+                settings.MEDIA_URL + 'audios'
+            )
+
+            audio = storage.save(f"{uuid.uuid4}.mp3", audio)
+            src = storage.url(audio)
+
+            attachment = AudioAttachment.objects.create(
+                src=src
+            )
+        
+        chat_message = ChatMessage.objects.create(
+            chat_id=chat_id,
+            body=body,
+            from_user_id=request.user.id,
+            attachment_code='FILE' if file else 'AUDIO' if audio else None,
+            attachment_id=attachment.id if attachment else None
+        )
+
+        chat_message_data = ChatMessageSerializer(chat_message).data
+
+        socket.emit('update_chat_message', {
+            "type": "create",
+            "message": chat_message_data,
+            "query": {
+                "chat_id": chat_id
+            }
+        })
+
+        Chat.objects.filter(id=chat_id).update(viewed_at=now())
+
+        socket.emit('update_chat', {
+            "query": {
+                "users": [chat.from_user_id, chat.to_user_id]
+            }
+        })
+
+        return Response({
+            "message": chat_message_data
+        })
+    
+class ChatMessageView(BaseView):
+    def delete(self, request, chat_id, message_id):
+        chat = self.chat_belongs_to_user(
+            user_id=request.user.id,
+            chat_id=chat_id
+        )
+
+        deleted = ChatMessage.objects.filter(
+            id=message_id,
+            chat_id=chat_id,
+            from_user_id=request.user.id,
+            deleted_at__isnull=True
+        ).update(
+            deleted_at=now()
+        )
+
+        if deleted:
+            socket.emit('update_chat_message', {
+                "type": "delete",
+                "query": {
+                    "chat_id": chat_id,
+                    "message_id": message_id
+                }
+            })
+
+        socket.emit('update_chat', {
+            "query": {
+                "users": [chat.from_user_id, chat.to_user_id]
+            }
+        })
+
+        return Response({
+            "success": True
+        })
