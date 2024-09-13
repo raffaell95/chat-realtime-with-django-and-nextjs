a9231d391141aa13f323a94d5e912e2278f2f620
Rafael Ribeiro <37004268+raffaell95@users.noreply.github.com>
2024-09-13 17:50:42 -0300

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from accounts.views import SignInView, SignUpView, UserView
-
-urlpatterns = [
-    path('signin', SignInView.as_view()),
-    path('signup', SignUpView.as_view()),
-    path('me', UserView.as_view())
-]
